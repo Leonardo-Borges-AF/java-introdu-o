bfee0cc34b0a22d76f58221dab20317ede6d66a6
Leonardo Borges <78118687+pikfavela@users.noreply.github.com>
2022-06-24 14:29:47 -0300

    Create Converte.java

---
+++ aulas 15-16/Ex 1-1/Converte.java
@@ -0,0 +1,13 @@
+public class Converte{
+  
+  public static double Km(int milhas){
+    
+    return milhas * 1.60934;
+  }
+
+  public static double L(int gal){
+    
+    return gal * 3.78541;
+  }
+  
+}
