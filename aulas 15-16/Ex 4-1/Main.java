c1d715058097ac470f741cffc321251428c76c0a
Leonardo Borges <78118687+pikfavela@users.noreply.github.com>
2022-06-24 14:34:29 -0300

    Create Main.java

---
+++ aulas 15-16/Ex 4-1/Main.java
@@ -0,0 +1,23 @@
+class Main {
+  public static void main(String[] args) {
+    
+    Cartao cartao = new Cartao();
+
+    System.out.println(cartao.getSaldo());
+    
+    cartao.Coffe();
+    cartao.Coffe();
+    cartao.Coffe();
+    cartao.Coffe();
+    cartao.Coffe();
+    cartao.Coffe();
+    cartao.Coffe();
+    cartao.Coffe();
+    cartao.Coffe();
+    cartao.Coffe();
+    cartao.Coffe();
+    
+    cartao.setSaldo(12);
+    System.out.println(cartao.getSaldo());
+  }
+}
